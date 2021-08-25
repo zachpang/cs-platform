a356b5e21873a6eba34427fd36d953ecc69c84a0
Zach Pang <zachery.pang@gmail.com>
2021-08-25 09:36:58 +0800

    Remove BasicAuthentication from DRF defaults

--- server/config/settings.py
+++ server/config/settings.py
@@ -74,6 +74,14 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 
+# Rest Framework
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ]
+}
+
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
