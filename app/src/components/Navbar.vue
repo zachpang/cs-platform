a7fe530af0d86371e63dcd66d754239bf6adb859
Zach Pang <zachery.pang@gmail.com>
2021-09-06 16:25:24 +0800

    feature: add Navbar to root app

---
+++ app/src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="container is-max-desktop mt-4">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">
+          <img src="../assets/logo-small.png" width="32" height="32" />
+          <span class="ml-1">CS Platform</span>
+        </router-link>
+      </div>
+    </nav>
+  </section>
+</template>
